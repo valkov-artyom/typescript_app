ed2942c61671486b901295a44e602d6477d8e890
Artem Valkov <valkov.artyom@gmail.com>
2020-08-04 22:52:12 +0300

    start project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import Hello from "./components/Hello";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello name="TypeScript" enthusiasmLevel={100} />,
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
